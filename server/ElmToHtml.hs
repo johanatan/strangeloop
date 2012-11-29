ec8005a108c44e3e80dc4ea9dd0e9104e4792fe1
Jonathan Leonard <johanatan@gmail.com>
2012-11-29 01:04:25 -0800

    Added ElmToHtml copyright.

--- server/ElmToHtml.hs
+++ server/ElmToHtml.hs
@@ -1,4 +1,5 @@
 {-
+Original:
 Copyright (c)2012, Evan Czaplicki
 
 All rights reserved.
@@ -29,6 +30,11 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-}
+
+{- Modifications:
+   Copyright (c) 2012. Jonathan Leonard.
+   All rights reserved.
 -}
 
 {-# LANGUAGE OverloadedStrings #-}
